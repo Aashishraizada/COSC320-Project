51de6685b12dc8c0863e5593f8ccfbfb70c87ccf
Aashish Raizada <60690063+Aashishraizada@users.noreply.github.com>
2021-04-09 19:37:01 -0700

    Final first implementation

--- BestRoutetInNetwork.java
+++ BestRoutetInNetwork.java
@@ -10,54 +10,47 @@ import java.util.PriorityQueue;
 public class BestRoutetInNetwork {
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner file_input = new Scanner(new File("flight_network.csv"));
+//		Scanner file_input = new Scanner(new File("flight_network2.csv"));
 		Scanner user_input = new Scanner(System.in);
 		int numFlights = 50;
-		int[][] flight_network = new int[50][6];
+		int[][] flight_network = new int[numFlights][6];
 		
-//		System.out.print("Please enter the starting location (enter numbers from 1 to 15 for now): ");
-		int source = 10;
-//		System.out.print("Please enter the destination location (enter numbers from 1 to 15 for now): ");
-		int destination = 1;
-//		System.out.print("Please enter the maximum number of stops you need (enter numbers from 1 to 4 for now): ");
-		int numStops = 5;
-		file_input.useDelimiter(",");
-		
-//		int count = 0;
-		for (int j = 0; j < 50; j++) {
-			for(int i = 0; i < 6; i++) {
-				flight_network[j][i] = Integer.parseInt(file_input.next());
-			}
-//			count++;
+		System.out.print("Please enter the starting location (enter numbers from 1 to 15 for now): ");
+		int source = user_input.nextInt();
+		while(source < 1 || source > 15) {
+			System.out.print("Invalid input. Try again: ");
+			source = user_input.nextInt();
 		}
-//		System.out.println(count);
-//		for(int j = 0; j < 50; j++) {
-//			for(int i = 0; i < 6; i++) {
-//				System.out.print(flight_network[j][i] + " ");
-//			}
-//			System.out.println();
-//		}
-//		int[][] flight_network = {{0, 1, 1850, 1430, 1400, 200},{1, 2, 2250, 1630, 1410, 400},{0, 2, 2420, 950, 850, 700}};
+		System.out.print("Please enter the destination location (enter numbers from 1 to 15 for now): ");
+		int destination = user_input.nextInt();
+		while(destination < 1 || destination > 15) {
+			System.out.print("Invalid input. Try again: ");
+			destination = user_input.nextInt();
+		}
 		
-		int[] result = algorithmA(numFlights, flight_network, source, destination, numStops);
-//		for(int i = 0; i < result.length; i++) {
-			System.out.printf("Starting location: %d\nNumber of stops in between: %d\nTotal price: $%d \nTotal duration: %d minutes\nTotal wait: %d minutes", result[0], result[1], result[2], result[3], result[4]);		
-//		}
+		loadDataA(numFlights, flight_network);
+		
+		int[] result = algorithmA(numFlights, flight_network, source, destination);
+		if(result != null) {
+			System.out.printf("Starting location: %d\nDestination: %d\nTotal price: $%d \nFlight duration: %d minutes\nWait duration: %d minutes\nTotal duration: %d minutes", source, result[0], result[1], result[2], result[3] - result[2], result[3]);		
+		}
+		else {
+			System.out.println("Empty result");
+		}
 		user_input.close();
-		file_input.close();
     }
 	
-	public static int[] algorithmA(int numFlights, int[][] flights, int source, int destination, int numStops) {
+	public static int[] algorithmA(int numFlights, int[][] flights, int source, int destination) {
 	
 		@SuppressWarnings("unchecked")
 		List<ConnectedNode>[] network = new ArrayList[numFlights];
 		for (int i = 0; i < numFlights; i++)
 			network[i] = new ArrayList<>(6);
 		for (int[] flight : flights)
-			network[flight[0]].add(new ConnectedNode(flight[1], flight[2], flight[3], flight[4], flight[5], numStops));
+			network[flight[0]].add(new ConnectedNode(flight[1], flight[2], flight[3], flight[4], flight[5]));
 		
 		PriorityQueue<ConnectedNode> heap = new PriorityQueue<>();
-		heap.add(new ConnectedNode(source, 0, 0, 0, 0, 0));
+		heap.add(new ConnectedNode(source, 0, 0, 0, 0));
 		
 		while (!heap.isEmpty()) {
 			ConnectedNode temp = heap.poll();
@@ -65,22 +58,26 @@ public class BestRoutetInNetwork {
 			int duration = temp.getDuration();
 			int wait = temp.getWait();
 			int price = temp.getPrice();
-//			int weight = temp.getCalc_weight();
-			int stopsMade = temp.getVertex();
-			if (stopsMade > numStops + 1)
-			    continue;
-			if (city == destination)
-			    return new int[] {city, stopsMade, price, duration, wait};
+			if (city == destination) {
+			    return new int[] {city, price, duration, wait};
+			}
+			
 			List<ConnectedNode> neighbors = network[city];
 			for (ConnectedNode neighbor : neighbors)
-				heap.add(new ConnectedNode(neighbor.getVertex(), duration + neighbor.getDuration(), wait + neighbor.getWait(), price + neighbor.getPrice(), stopsMade + 1));
+				heap.add(new ConnectedNode(neighbor.getVertex(), duration + neighbor.getDuration(), wait + neighbor.getWait(), price + neighbor.getPrice()));
 		}
 
-		
-		return new int[] {10, 1, 346, 582, 263, 901};
+		return null;
 	}
 
-    public static void loadDataA() {
-		
+    public static void loadDataA(int numFlights, int[][] flight_network) throws FileNotFoundException {
+		Scanner file_input = new Scanner(new File("flight_network.csv"));
+		file_input.useDelimiter(",");
+		for (int j = 0; j < numFlights; j++) {
+			for(int i = 0; i < 6; i++) {
+				flight_network[j][i] = Integer.parseInt(file_input.next());
+			}
+		}
+		file_input.close();
 	}
 }
